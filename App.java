b9fe7a268ad7c72876b22f52495b1a3cdf16408a
humaira-afrin <147653772+humaira-afrin@users.noreply.github.com>
2024-03-31 13:21:14 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+
+
+        int boardWidth = 600;
+        int boardHeight= boardWidth;
+
+
+        JFrame frame = new JFrame ("Snake");
+        frame.setVisible (true);
+        frame.setSize (boardWidth, boardHeight);
+        frame.setLocationRelativeTo (null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Snakegame snakeGame = new Snakegame(boardWidth, boardHeight);
+        frame.add(snakeGame);
+        frame.pack();
+        snakeGame.requestFocus();
+        
+
+    }
+}
